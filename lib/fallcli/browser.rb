e7feb0cbb81580f9dcfaa2601c8b7c14429c242e
Peter Souter <peter.souter@puppetlabs.com>
2015-01-24 13:39:04 +0000

    Adds Browser class with text display helpers
    Refactored from example given by @radubogdan: http://stackoverflow.com/questions/27771764/curses-array-browsing-with-keyboard-in-ruby :+1:

---
+++ lib/fallcli/browser.rb
@@ -0,0 +1,29 @@
+module FallCli
+  class Browser
+    attr_accessor :position
+
+    def initialize items
+      @files = Array.new
+      items.each { |item| @files << item }
+      @position = 0
+    end
+
+    def show_files
+      @files.each_with_index.map do |item, index|
+        position == index ? "[#{pretty_dropbox_file(item)}]" : " #{pretty_dropbox_file(item)} "
+      end
+    end
+
+    def pretty_dropbox_file file
+      "#{file.path} | #{file.mime_type} | #{file.size}"
+    end
+
+    def position_up
+      @position -= 1 unless @position < 1
+    end
+
+    def position_down
+      @position += 1 unless @position == (@files.size - 1)
+    end
+  end
+end
